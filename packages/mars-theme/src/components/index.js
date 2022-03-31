868dac53c4edf020805ec21687e4880d7e3f475c
Zakir Sajib <zakirsajib@yahoo.co.uk>
2022-03-31 10:30:45 +0600

    Update index.js

--- packages/mars-theme/src/components/index.js
+++ packages/mars-theme/src/components/index.js
@@ -56,9 +56,7 @@ const Theme = ({ state }) => {
       on the type of URL we are in. */}
       <Main id={ `_totalResults${data.total}` } className={ `_authorName${data.isAuthor}` }>
         <Switch>
-          <Loading when={data.isFetching} />
-          <ListHome when={data.isHome} />
-          <ListCat when={data.isArchive} />
+          
           <Post when={data.isPostType} />
           <PageError when={data.isError} />
         </Switch>
